ba17d51b49df41520993e84908f3aae6008389ec
saifaldin hossam <actually.saif@gmail.com>
2025-12-09 00:42:47 +0200

    footer done

---
+++ localStorage.js
@@ -0,0 +1,8 @@
+let characters = [{
+    charName: 'David Martinez',
+    charAge: '18',
+    backstory: `David Martinez grew up in Santo Domingo (Heywood) with his mother, Gloria. Despite struggling financially, she enrolled him in the prestigious Arasaka Academy, hoping to give him a chance at a better life. After her death in a drive-by incident, David was left alone, grieving, angry, and unable to afford school. In desperation — and driven by his desire to prove himself — David installed a stolen Sandevistan military cyberware, a risky and highly illegal act. This caught the attention of Lucy and the Edgerunner crew, who pulled him into the world of merc work, gigs, and high-risk cyber-enhancements. David's natural talent with the Sandevistan pushed him into the center of Night City's violent power struggles.`,
+    aff1: 'Edgerunners', 
+    aff2: 'afterlife mercenaries',
+    aff2: 'arasaka (formerly)'
+}]
